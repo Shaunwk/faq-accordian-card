c48753b8f4f347b2d9e485d770aad913ee85e64b
Shaun Kelly <shaunwilliamkelly@gmail.com>
2023-05-11 01:40:53 -0700

    added media query for desktop

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ const accordianItem = document.querySelectorAll('.accordian-item');
 accordianItem.forEach(function(item){
   const question = item.querySelector('.accordian-question');
   const answer = item.querySelector('.accordian-answer');
+  const arrow = item.querySelector('.accordian-arrow');
 
   question.addEventListener('click', function(){
     if(answer.style.display === 'none'){
       question.classList.add('active'); //so I can make the question turn black when the answer is out
       answer.style.display = 'block';
+      arrow.style.transform = 'rotate(180deg)';
     } else {
       question.classList.remove('active');
       answer.style.display = 'none';
+      arrow.style.transform = 'rotate(0deg)';
     }
   })
+
+  
 
 });

5ef1e1b4bc6f77daca7e6ea0907a218b86880fea
Shaun Kelly <shaunwilliamkelly@gmail.com>
2023-05-10 23:04:04 -0700

    added some functionality with JS and re did the HTML & CSS

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-// const questions = document.querySelector('.questions');
-// const answers = document.querySelector('.answers');
-// const arrow = document.querySelector('.arrow');
+const accordianItem = document.querySelectorAll('.accordian-item');
 
-// questions.addEventListener('click', function(){
-//     if(answers.style.display === 'none'){
-//         answers.style.display = 'block';
-//     } else {
-//         answers.style.display = 'none';
-//     }
-// });
+accordianItem.forEach(function(item){
+  const question = item.querySelector('.accordian-question');
+  const answer = item.querySelector('.accordian-answer');
 
-
-//Chat GPT reccomended code//
-
-const questionElements = document.querySelectorAll('.questions');
-const arrowElements = document.querySelectorAll('.arrow');
-
-questionElements.forEach(function(question) {
-  question.addEventListener('click', function() {
-    const answer = this.querySelector('.answers');
-    if (answer.style.display === 'none') {
+  question.addEventListener('click', function(){
+    if(answer.style.display === 'none'){
+      question.classList.add('active'); //so I can make the question turn black when the answer is out
       answer.style.display = 'block';
     } else {
+      question.classList.remove('active');
       answer.style.display = 'none';
     }
-  });
-});
+  })
 
-/*By using querySelectorAll, the code selects 
-all elements with the class name 'questions' 
-and iterates over them using the forEach method.
-Inside the event listener function, it selects 
-the corresponding answer element using 
-querySelector('.answers') on the clicked question element. 
-*/
+});
